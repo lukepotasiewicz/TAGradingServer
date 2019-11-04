f3d5e51ea2728a55f80be05f5db01df057179809
Luke Potasiewicz <lpotasiewicz@tesla.com>
2019-11-03 19:06:57 -0500

    created cert keys and then didnt use them :)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 4242;
 app.use(cors());
 
 const readDatabase = () => JSON.parse(fs.readFileSync('database.json'));
