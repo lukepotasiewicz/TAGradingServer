7cc881a3ce5492995dc52eb07784f8ed3a5235cd
Luke Potasiewicz <lpotasiewicz@tesla.com>
2019-11-08 18:03:50 -0500

    working silk server update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/opt/nodejs/v10/bin/node
 
 const http = require('unit-http');
+const url = require('url');
 const fs = require('fs');
 const baseUrl = '/proxyserver';
 const app = {};
@@ -8,36 +9,41 @@ const app = {};
 const readDatabase = () => JSON.parse(fs.readFileSync('database.json'));
 
 const writeDatabase = (data) => {
-    const oldData = readDatabase();
-    fs.writeFileSync('database.json',  JSON.stringify({...oldData, ...data}));
+  const oldData = readDatabase();
+  fs.writeFileSync('database.json', JSON.stringify({...oldData, ...data}));
 };
 
 app[baseUrl] = (req, res) => res.write("welcome to my proxy server");
 
 app[baseUrl + '/health'] = (req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify({healthy: true}));
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify({healthy: true}));
 };
 
 /* ----------- GET ----------- */
 app[baseUrl + '/getData'] = (req, res) => {
-    const data = readDatabase();
-    return res.status(200).json(data);
+  const data = readDatabase();
+  return res.write(JSON.stringify(data));
 };
 
 /* ----------- UPDATED ----------- */
 app[baseUrl + '/updateData'] = (req, res) => {
-    if (!req.query.data) return res.status(400).json({error: "Bad request"});
-    writeDatabase(JSON.parse(req.query.data));
-    return res.status(200).json({queryString: req.query});
+  const {query} = url.parse(req.url, true);
+  if (!query.data) return res.write(JSON.stringify({error: "Bad request"}));
+  writeDatabase(JSON.parse(query.data));
+  return res.write(JSON.stringify({success: true}));
 };
 
 http.createServer(function (req, res) {
-    try {
-        app[req.url](req, res);
-        res.end();
-    }
-    catch (e) {
-        res.end(e);
-    }
+  try {
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    });
+    const {pathname} = url.parse(req.url, true);
+    app[pathname](req, res);
+    res.end();
+  } catch (e) {
+    res.end('ERROR: ' + e);
+  }
 }).listen();
